7138fdcfb59be41415f65b21b1a02141d980d07f
Negasi <NegasiHaile.Abadi@gmail.com>
2022-08-19 09:06:06 +0300

    Added a description on the home page

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -11,7 +11,7 @@ import { CategoriesList } from "../Components/Categories";
 import { Box, Button, HStack, Icon, ScrollView, Text } from "native-base";
 
 // Icons
-import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Home = ({ navigation }) => {
   return (
@@ -50,6 +50,87 @@ const Home = ({ navigation }) => {
                 </Text>
               </HStack>
               <CategoriesList />
+            </Box>
+
+            <Box mb={10}>
+              <Text
+                fontSize="2xl"
+                color="coolGray.500"
+                mb={3}
+                textAlign="center"
+              >
+                Why should you Sell and Purchase here?
+              </Text>
+              <HStack justifyContent="space-around" alignItems="center" my={3}>
+                <Box alignItems="center" w="50%" px={2}>
+                  <Box bg="primary.50" borderRadius={"full"} p={1}>
+                    <Icon
+                      size="3xl"
+                      color="primary.500"
+                      as={<MaterialCommunityIcons name={"currency-usd"} />}
+                    />
+                  </Box>
+                  <Text fontSize="sm" bold color="coolGray.500">
+                    Low post fee
+                  </Text>
+                  <Text textAlign="center" fontSize="xs">
+                    Here, You are allowed to post and sell your products is very
+                    low cost.{" "}
+                  </Text>
+                </Box>
+                <Box alignItems="center" w="50%" px={2}>
+                  <Box bg="primary.50" borderRadius={"full"} p={1}>
+                    <Icon
+                      size="3xl"
+                      color="primary.500"
+                      as={<MaterialIcons name={"local-shipping"} />}
+                    />
+                  </Box>
+                  <Text fontSize="sm" bold color="coolGray.500">
+                    Free shipping
+                  </Text>
+                  <Text textAlign="center" fontSize="xs">
+                    Here, You are allowed to post and sell your products is very
+                    low cost.{" "}
+                  </Text>
+                </Box>
+              </HStack>
+              <HStack justifyContent="space-around" alignItems="center" my={3}>
+                <Box alignItems="center" w="50%" px={2}>
+                  <Box bg="primary.50" borderRadius={"full"} p={1}>
+                    <Icon
+                      size="3xl"
+                      color="primary.500"
+                      as={<MaterialIcons name={"supervised-user-circle"} />}
+                    />
+                  </Box>
+                  <Text fontSize="sm" bold color="coolGray.500">
+                    User to user market
+                  </Text>
+                  <Text textAlign="center" fontSize="xs">
+                    Here, You are allowed to post and sell your products is very
+                    low cost.{" "}
+                  </Text>
+                </Box>
+                <Box alignItems="center" w="50%" px={2}>
+                  <Box bg="primary.50" borderRadius={"full"} p={1}>
+                    <Icon
+                      size="3xl"
+                      color="primary.500"
+                      as={
+                        <MaterialCommunityIcons name="book-variant-multiple" />
+                      }
+                    />
+                  </Box>
+                  <Text fontSize="sm" bold color="coolGray.500">
+                    Everything availabe
+                  </Text>
+                  <Text textAlign="center" fontSize="xs">
+                    Here, You are allowed to post and sell your products is very
+                    low cost.{" "}
+                  </Text>
+                </Box>
+              </HStack>
             </Box>
           </Box>
         </Box>
