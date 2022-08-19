50f89c2333d2c114339ac03452a9c74ac4c0b439
Negasi <NegasiHaile.Abadi@gmail.com>
2022-08-19 11:37:55 +0300

    Added api setup

---
+++ src/API/index.js
@@ -0,0 +1,21 @@
+import {
+  api_getAllUsers,
+  api_getUserProfile,
+  api_editUserProfile,
+  api_ChangeMyPassword,
+  api_scheduleNotification,
+  api_UpdateNotificationStatusToSeen,
+  api_DeleteUserNotification,
+} from "./users"; // User APIs
+
+// Exporting
+// users
+export {
+  api_getAllUsers,
+  api_getUserProfile,
+  api_editUserProfile,
+  api_ChangeMyPassword,
+  api_scheduleNotification,
+  api_UpdateNotificationStatusToSeen,
+  api_DeleteUserNotification,
+};
