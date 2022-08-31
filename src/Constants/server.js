bca6ef6ad99310d56c138c7568c84b4606aeab2c
Negasi <NegasiHaile.Abadi@gmail.com>
2022-08-31 11:55:00 +0300

    Added user contacts in product detail

--- src/Constants/server.js
+++ src/Constants/server.js
@@ -1,5 +1,5 @@
 const PORT = 5055;
 
-export const server = `http://192.168.1.32:${PORT}`;
+export const server = `http://192.168.137.1:${PORT}`;
 
 // export const server = `http://localhost:${PORT}`;
--- src/Screens/ProductDetail.js
+++ src/Screens/ProductDetail.js
@@ -6,10 +6,10 @@ import { FeaturedProducts } from "../Components/FeaturedProducts";
 import { ProductGalleryView } from "../Components/Product";
 
 // Native-base components
-import { Box, Button, HStack, Icon, Text } from "native-base";
+import { Box, Button, HStack, Icon, Text, VStack } from "native-base";
 
 // Icons
-import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
 
 const ProductDetail = ({ route, navigation }) => {
   const product = route.params;
@@ -18,6 +18,27 @@ const ProductDetail = ({ route, navigation }) => {
     <ContentContainer>
       <Box mt={10} w="90%">
         <ProductGalleryView product={product} />
+
+        <Box py={3} >
+          <Text>Product-Name : This is name</Text>
+
+          <Box>
+            <Text fontSize={'lg'} color='muted.500' my={2}>Contacts </Text>
+            <HStack justifyContent={'space-between'} w='100%' >
+              <VStack alignItems="center" w="50%">
+          <Button size="xs"p={2} my={1}  borderRadius="full" bg="success.700" w="90%"
+          leftIcon={<Icon as={MaterialIcons}  name="call"  />}
+          > +251936373144 </Button>
+          <Button size="xs"p={2} my={1} borderRadius="full" bg="success.400" w="90%"
+           leftIcon={<Icon as={MaterialCommunityIcons}  name="whatsapp"  />} textAlign="left"> Whatsapp </Button>
+          </VStack>
+          <VStack alignItems="center"  w="50%">
+          <Button size="xs"p={2} my={1}  borderRadius="full" bg="success.700"  w="90%"> +251936373144 </Button>
+          <Button size="xs"p={2} my={1} borderRadius="full" bg="success.400" w="90%"> Whatsapp </Button>
+          </VStack>
+          </HStack>
+          </Box>
+        </Box>
 
         <HStack justifyContent="space-between" alignItems="center">
           <Text fontSize="2xl" color="coolGray.500">
