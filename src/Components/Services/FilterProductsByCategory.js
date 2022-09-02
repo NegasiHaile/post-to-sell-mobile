8f1c39639077d94d944a5426060e5e21ab94cea7
Negasi <NegasiHaile.Abadi@gmail.com>
2022-09-02 09:28:25 +0300

    Moved to the serc root directory

--- src/Components/Services/FilterProductsByCategory.js
+++
@@ -1,6 +0,0 @@
-export const FilterProductsByCategory = (products, categoryId) => {
-  const productsByCategory = products?.filter(
-    (product) => product.category === categoryId
-  );
-  return productsByCategory;
-};
--- src/Components/Services/index.js
+++
@@ -1,3 +0,0 @@
-import { FilterProductsByCategory } from "./FilterProductsByCategory";
-
-export { FilterProductsByCategory };
