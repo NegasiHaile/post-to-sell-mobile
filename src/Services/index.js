f4949cc332f464fa208b74e731031459af1afc10
Negasi <NegasiHaile.Abadi@gmail.com>
2022-09-02 09:14:31 +0300

    Moved the Services folder from the components forlder back to src

---
+++ src/Services/index.js
@@ -0,0 +1,4 @@
+import { FilterProductsByCategory } from "./FilterProductsByCategory";
+import { filterCategoryName } from "./FilterCategoryName";
+
+export { FilterProductsByCategory, filterCategoryName };
