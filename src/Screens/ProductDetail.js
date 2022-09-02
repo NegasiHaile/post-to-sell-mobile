9899fa760cd3e0f13bcce1080153172eaa5db003
Negasi <NegasiHaile.Abadi@gmail.com>
2022-09-02 09:34:41 +0300

    Updated product detail

--- src/Screens/ProductDetail.js
+++ src/Screens/ProductDetail.js
@@ -1,4 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
+import { Linking, Platform } from "react-native";
+import { useSelector } from "react-redux";
+
+//Service provider functions
+import {
+  filterCategoryName,
+  filterSubCategoryName,
+  filterBrandName,
+} from "../Services/FilterCategoryName";
+
+// Utility functions
+import { ShortDateFormat } from "../Utils";
 
 // Dev components
 import { ContentContainer } from "../Components/Containers";
@@ -9,37 +21,146 @@ import { ProductGalleryView } from "../Components/Product";
 import { Box, Button, HStack, Icon, Text, VStack } from "native-base";
 
 // Icons
-import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  FontAwesome5,
+  MaterialIcons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 
 const ProductDetail = ({ route, navigation }) => {
   const product = route.params;
+  const categories = useSelector((state) => state.categories);
+
+  console.warn(product);
+  const onPressMobileNumberClick = (number) => {
+    let phoneNumber = "";
+    if (Platform.OS === "android") {
+      phoneNumber = `tel:${number}`;
+    } else {
+      phoneNumber = `telprompt:${number}`;
+    }
+
+    Linking.openURL(phoneNumber);
+  };
+
+  const onPressOpenWhatsapp = (number, productName) => {
+    const sampleMessage = `Hi, I see your product named as ${productName} in post-to-sell, Is it available now?`;
+    Linking.openURL(`whatsapp://send?phone=${number}&text= ${sampleMessage}`);
+  };
 
   return (
     <ContentContainer>
       <Box mt={10} w="90%">
-        <ProductGalleryView product={product} />
+        <Box mb={20}>
+          <ProductGalleryView product={product} />
 
-        <Box py={3} >
-          <Text>Product-Name : This is name</Text>
+          <Box py={3}>
+            <Box py={2}>
+              <Text fontSize={"lg"} color="muted.500" my={2}>
+                Product detial
+              </Text>
 
-          <Box>
-            <Text fontSize={'lg'} color='muted.500' my={2}>Contacts </Text>
-            <HStack justifyContent={'space-between'} w='100%' >
-              <VStack alignItems="center" w="50%">
-          <Button size="xs"p={2} my={1}  borderRadius="full" bg="success.700" w="90%"
-          leftIcon={<Icon as={MaterialIcons}  name="call"  />}
-          > +251936373144 </Button>
-          <Button size="xs"p={2} my={1} borderRadius="full" bg="success.400" w="90%"
-           leftIcon={<Icon as={MaterialCommunityIcons}  name="whatsapp"  />} textAlign="left"> Whatsapp </Button>
-          </VStack>
-          <VStack alignItems="center"  w="50%">
-          <Button size="xs"p={2} my={1}  borderRadius="full" bg="success.700"  w="90%"> +251936373144 </Button>
-          <Button size="xs"p={2} my={1} borderRadius="full" bg="success.400" w="90%"> Whatsapp </Button>
-          </VStack>
-          </HStack>
+              <Text>{product ? ShortDateFormat(product.createdAt) : ""}</Text>
+              <HStack justifyContent="space-between" alignItems="center">
+                <Text bold>
+                  Name:{" "}
+                  {product?.productName ? product?.productName : "Unnamed"}
+                </Text>
+                <Text bold>
+                  Price:{" "}
+                  {product?.currentPrice
+                    ? `${product?.currentPrice} ETB`
+                    : "$ Call me"}
+                </Text>
+              </HStack>
+              <Text bold py={1}>
+                {(categories, product.model)}{" "}
+                {filterBrandName(categories, product.brand)}{" "}
+                {filterSubCategoryName(categories, product.subCategory)}{" "}
+                {filterCategoryName(categories, product.category)}
+              </Text>
+              <Box my={2}>
+                {product?.discription && (
+                  <Text py={2}>{product?.discription}</Text>
+                )}
+              </Box>
+            </Box>
+            <Box py={2}>
+              <Text fontSize={"lg"} color="muted.500" my={2}>
+                Contacts
+              </Text>
+              <HStack justifyContent={"space-between"} w="100%">
+                <VStack alignItems="center" w="50%">
+                  <Button
+                    size="xs"
+                    p={2}
+                    my={1}
+                    borderRadius="full"
+                    bg="success.700"
+                    w="90%"
+                    leftIcon={<Icon as={MaterialIcons} name="call" />}
+                    onPress={() =>
+                      onPressMobileNumberClick(product.contacts.phoneNumber)
+                    }
+                  >
+                    {product.contacts.phoneNumber}
+                  </Button>
+                  <Button
+                    size="xs"
+                    p={2}
+                    my={1}
+                    borderRadius="full"
+                    bg="success.500"
+                    w="90%"
+                    leftIcon={
+                      <Icon as={MaterialCommunityIcons} name="whatsapp" />
+                    }
+                    textAlign="left"
+                    onPress={() =>
+                      onPressOpenWhatsapp(
+                        product.contacts.whatsapp,
+                        product.productName
+                      )
+                    }
+                  >
+                    Whatsapp
+                  </Button>
+                </VStack>
+                <VStack alignItems="center" w="50%">
+                  <Button
+                    size="xs"
+                    p={2}
+                    my={1}
+                    borderRadius="full"
+                    bg="blue.700"
+                    w="90%"
+                    leftIcon={<Icon as={MaterialIcons} name="facebook" />}
+                    onPress={() => Linking.openURL("https://facebok.com/")}
+                  >
+                    facebook
+                  </Button>
+                  <Button
+                    size="xs"
+                    p={2}
+                    my={1}
+                    borderRadius="full"
+                    bg="blue.400"
+                    w="90%"
+                    leftIcon={<Icon as={FontAwesome5} name="telegram" />}
+                    onPress={() =>
+                      Linking.openURL(
+                        `https://t.me/${product.contacts?.telegram}`
+                      )
+                    }
+                  >
+                    telegram
+                  </Button>
+                </VStack>
+              </HStack>
+            </Box>
           </Box>
         </Box>
-
+        {/* Featured products list */}
         <HStack justifyContent="space-between" alignItems="center">
           <Text fontSize="2xl" color="coolGray.500">
             Featured Products
@@ -52,7 +173,6 @@ const ProductDetail = ({ route, navigation }) => {
             Show all
           </Button>
         </HStack>
-        {/* Featured products list */}
         <FeaturedProducts />
       </Box>
     </ContentContainer>
