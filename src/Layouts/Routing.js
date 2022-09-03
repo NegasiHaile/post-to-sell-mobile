6284213fb1e61c8a323cd5727abbaaa49a0d0180
Negasi <NegasiHaile.Abadi@gmail.com>
2022-09-03 11:14:45 +0300

    Added user authorization to screen routings

--- src/Layouts/Routing.js
+++ src/Layouts/Routing.js
@@ -37,46 +37,72 @@ const Routing = ({ theme }) => {
           },
         }}
       >
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Signin"
-          component={Signin}
-          options={{
-            title: "Sign In",
-          }}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={Signup}
-          options={{
-            title: "Sign Up",
-          }}
-        />
-        <Stack.Screen
-          name="ForgetPassword"
-          component={ForgetPassword}
-          options={{
-            title: "Forget Password",
-          }}
-        />
-        <Stack.Screen
-          name="Products"
-          component={Products}
-          options={{
-            title: "All Products",
-          }}
-        />
-        <Stack.Screen
-          name="ProductDetail"
-          component={ProductDetail}
-          options={{
-            title: "Product Detail",
-          }}
-        />
+        {"accesstoken" in profile ? (
+          <>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Products"
+              component={Products}
+              options={{
+                title: "All Products",
+              }}
+            />
+            <Stack.Screen
+              name="ProductDetail"
+              component={ProductDetail}
+              options={{
+                title: "Product Detail",
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Products"
+              component={Products}
+              options={{
+                title: "All Products",
+              }}
+            />
+            <Stack.Screen
+              name="ProductDetail"
+              component={ProductDetail}
+              options={{
+                title: "Product Detail",
+              }}
+            />
+            <Stack.Screen
+              name="Signin"
+              component={Signin}
+              options={{
+                title: "Sign In",
+              }}
+            />
+            <Stack.Screen
+              name="Signup"
+              component={Signup}
+              options={{
+                title: "Sign Up",
+              }}
+            />
+            <Stack.Screen
+              name="ForgetPassword"
+              component={ForgetPassword}
+              options={{
+                title: "Forget Password",
+              }}
+            />
+          </>
+        )}
       </Stack.Navigator>
       <BottomNav />
     </NavigationContainer>
